29256f91af1233d2b3c05fe01b6326aa9e52121b
Kylmalcolm <kylie.a.malcolm@gmail.com>
2020-10-09 16:54:39 -0500

    Homework ver.2

--- PyBank/Homework/PyBankHW.py
+++ PyBank/Homework/PyBankHW.py
@@ -15,19 +15,25 @@ with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
 
+    dates = []
     profits = []
+    counter = 0
+    total = 0
     for row in csvreader:
-        profit = int(row[1])
-        profits.append(profit)
-        print(sum(profits))
-        print(len(profits))
+        date = row[0]
+        dates.append(date)
+        profit = row[1]
+        profits.append(int(profit))
+        counter = counter +1
+        total = total + int(profit)
 
-
-
-#months = sum(1 for row in csvreader)
-#print(months)
-
-
+print("Financial Analysis")
+print("----------------------------")
+print("Total Months: " + str(counter))
+print("Total: $" + str(total))
+print("Average Change: $")
+print("Greatest Increase in Profits: ")
+print("Greatest Decrease in Profits: ")
 
         #in a loop, create a varaible outside of loop, set to zero,
         # and add 1 every time I'm in the loop
