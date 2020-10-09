0f67eef039d8bd0cfd4c85c461c70a09907f0e90
Kylmalcolm <kylie.a.malcolm@gmail.com>
2020-10-09 18:32:19 -0500

    almost done with PyBank

--- PyBank/Homework/PyBankHW.py
+++ PyBank/Homework/PyBankHW.py
@@ -17,23 +17,49 @@ with open(budget_csv, 'r') as csvfile:
 
     dates = []
     profits = []
+    changes = []
+
     counter = 0
     total = 0
+    increase = 0
+    decrease = 0
+
     for row in csvreader:
         date = row[0]
         dates.append(date)
+
         profit = row[1]
         profits.append(int(profit))
+
         counter = counter +1
         total = total + int(profit)
+
+        if int(increase) > int(profit):
+            increase = increase
+        else:
+            increase = int(profit)
+            increase_date = date
+
+        if int(decrease) < int(profit):
+            decrease = decrease
+        else:
+            decrease = int(profit)
+            decrease_date = date
+        
+    #for profit in profits_list:
+        #change = 
 
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(counter))
 print("Total: $" + str(total))
 print("Average Change: $")
-print("Greatest Increase in Profits: ")
-print("Greatest Decrease in Profits: ")
+print("Greatest Increase in Profits: " + str(increase_date) + " $" + str(increase))
+print("Greatest Decrease in Profits: " + str(decrease_date) + " $" + str(decrease))
+
+print(changes)
+
+#DON'T FORGET THE EXPORT STEP!!!!!
 
         #in a loop, create a varaible outside of loop, set to zero,
         # and add 1 every time I'm in the loop
